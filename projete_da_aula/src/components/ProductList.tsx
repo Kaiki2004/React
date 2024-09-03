dba80782fb9f7336701392a8c6fc1f47718c0f41
Kaiki Andrade <125385416+Kaiki2004@users.noreply.github.com>
2024-09-02 22:11:18 -0300

    Add files via upload

---
+++ projete_da_aula/src/components/ProductList.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import '../stylesheet/styleesheet.css';
+import { getProducts, deleteProduct } from '../service/api';
+import { Link } from 'react-router-dom';
+interface Product {
+    id: string;
+    name: string;
+    description: string;
+    genero: string;
+    classificacao: string;
+    diretor: string;
+}
+function ProductList() {
+    const [products, setProducts] = useState<Product[]>([]);
+    useEffect(() => {
+        loadProducts();
+    }, []);
+    const loadProducts = async () => {
+        const response = await getProducts();
+        setProducts(response.data);
+    };
+    const handleDelete = async (id: string) => {
+        await deleteProduct(id);
+        loadProducts();
+    };
+    return (
+        <div>
+            <h1> Filmes</h1>
+            <Link to="/add">Add Filme</Link>
+            <ul>
+                {products.map((product) => (
+                    <li key={product.id}>
+                        {product.name} - {product.genero} - {product.classificacao} - {product.diretor} units
+                        <Link to={`/edit/${product.id}`}>Edit</Link>
+                        <button onClick={() => handleDelete(product.id)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+export default ProductList; 
---
+++ projete_da_aula/src/pages/AddProduct.tsx
@@ -0,0 +1,11 @@
+import ProductForm from '../components/ProductForm';
+import '../stylesheet/styleesheet.css';
+function AddProduct() {
+ return (
+ <div>
+ <h1>Adicione um Filme</h1>
+ <ProductForm />
+ </div>
+ );
+}
+export default AddProduct;
